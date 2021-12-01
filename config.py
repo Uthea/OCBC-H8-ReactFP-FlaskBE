23db6f3048c530a328fa5e9ce13c7152d2e7bafa
Uthea <gstsadhu@gmail.com>
2021-12-01 07:52:56 +0700

    use postgresql instead

--- config.py
+++ config.py
@@ -4,8 +4,17 @@ from datetime import timedelta
 basedir = os.path.dirname(os.path.realpath(__file__))
 
 
+def get_db_url():
+    db_url = os.getenv('DATABASE_URL')
+    if db_url:
+        return db_url.replace("postgres", "postgresql")  # heroku prefix db url with postgres instead of postgresql
+
+    return db_url
+
+
 class BaseConfig(object):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_be_jwt.db')
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_be_jwt.db')
+    SQLALCHEMY_DATABASE_URI = get_db_url()
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     JWT_TOKEN_LOCATION = ['headers']
